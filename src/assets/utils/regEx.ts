bbb71ef14cab48f3d6e43587342f1523ac56e8f5
RalfiSlask <nilssonnarki@gmail.com>
2023-11-28 15:41:07 +0100

    fixed regExText to include swedish characters

--- src/assets/utils/regEx.ts
+++ src/assets/utils/regEx.ts
@@ -1,6 +1,7 @@
-export const textOnlyRegx = /^[A-Za-z\s]+$/;
+export const textOnlyRegx = /^[A-Za-zåäöÅÄÖ\s]+$/;
 export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-export const telephoneRegex = /^\+?[0-9]{1,4}?[-. ]?(\([0-9]{1,3}\)|[0-9]{1,4})[-. ]?[0-9]{1,4}[-. ]?[0-9]{1,4}[-. ]?[0-9]{1,9}$/;
+export const telephoneRegex =
+  /^\+?[0-9]{1,4}?[-. ]?(\([0-9]{1,3}\)|[0-9]{1,4})[-. ]?[0-9]{1,4}[-. ]?[0-9]{1,4}[-. ]?[0-9]{1,9}$/;
 export const numberOnlyRegex = /^[0-9]+$/;
 export const socialSecurityRegex = /\d{6}-\d{4}/;
 export const addressRegex = /\w+ \w+(?:[-' \w]*)*(?:,? \w+)?/;
